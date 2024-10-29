e5682deb51977aced3d6f6c6c97418b4a6dcd151
Ibrahim Ali <ibrahimali05757@gmail.com>
2024-10-29 11:30:12 +0000

    Update app.py

--- Weather API/app.py
+++ Weather API/app.py
@@ -3,7 +3,7 @@ import requests
 
 app = Flask('Weather Dashboard')
 
-API_KEY = '7ab4f879ef0804ca67aa589e96c78e27'
+API_KEY = ''
 
 def get_weather_data(city):
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric'
